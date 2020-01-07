3ccdcfd41779f83871df6c3f6cc82055b26ccbac
ramandeepformay <ramandeepformay@gmail.com>
2020-01-06 19:01:07 -0500

    switch component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Home from "./Home"
 import About from "./About"
 import Header from "./Header"
 import Teachers from "./Teachers"
 import Courses from "./Courses"
+import NotFound from "./NotFound"
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
       <Header/>
-        <Route exact path ="/" component={Home}/>
-        <Route path ="/about" render={()=><About title=" Courses"/>}/>
-        <Route path ="/teachers" component ={Teachers}/>
-        <Route path = "/courses" component ={Courses}/>   
+        <Switch>
+          <Route exact path ="/" component={Home}/>
+          <Route path ="/about" render={()=><About title=" Courses"/>}/>
+          <Route path ="/teachers" component ={Teachers}/>
+          <Route path = "/courses" component ={Courses}/>   
+          <Route component ={NotFound} />
+        </Switch>
     </div>
   </BrowserRouter>
   
